1f6cccf774ac3c0efecda2f439df932ab089e193
Michael Borinsky <borinsky@physik.hu-berlin.de>
2015-07-19 22:32:28 +0200

    Working copy

--- fabula.py
+++ fabula.py
@@ -1,36 +1,49 @@
 
 import sys
-import yaml
+import yaml, json
 
 import mistune
 from jinja2 import Template
 
 import argparse
 
+class RenderManager:
+    def __init__(self):
+        self._link_dict = dict()
+
+    def getRenderer(self, scene):
+        link_dict = self._link_dict
+        class FabulaRenderer( mistune.Renderer ):
+            def link(self, link, title, text):
+                next_id = len(link_dict)
+                identifier = "link%d" % next_id
+                link_dict[identifier] = ( scene, link )
+
+                return '<a id="%s" class="flink" href="#">%s</a>' % (identifier, text)
+
+        renderer = FabulaRenderer()
+        md = mistune.Markdown( renderer=renderer )
+        return md
+
 class Scene:
-    def __init__( self, name, content, **kwargs ):
-        self.name = name
-        self.content = content
+    def __init__( self, identifier, name, content, **kwargs ):
+        self._name = name
+        self._content = content
+
+        self._identifier = identifier
 
     def render( self, md ):
-        pass
+        html_scene = md.render(self._content)
 
-def renderScenes( raw_scenes ):
-    md = mistune.Markdown()
-
-    def renderScene( name, attrib ):
         out = dict()
-
-        content = attrib['content']
-        html_scene = md.render(content)
-
-        out['id'] = 0
-        out['name'] = name
+        out['id'] = self._identifier
+        out['name'] = self._name
         out['content'] = html_scene
 
         return out
 
-    return [ renderScene( *item ) for item in raw_scenes.iteritems() ] 
+def getScenesFromYAML( raw_scenes ):
+    return { name : Scene( "scene%d" % i, name, **attribs ) for i, (name, attribs) in enumerate(raw_scenes.iteritems()) }
 
 def main():
     frame_file = "frame.html"
@@ -45,15 +58,28 @@ def main():
     html_out_file = args.output
 
     with open(syu_in_file, "r") as i:
-        raw_scenes = yaml.load( i.read() )['scenes']
+        yaml_scenes = yaml.load( i.read() )['scenes']
 
-    scenes = renderScenes( raw_scenes )
+    scene_dict = getScenesFromYAML( yaml_scenes )
+    renderManager = RenderManager()
+
+    scenes_contexts = [ 
+        scene.render(renderManager.getRenderer(name)) 
+                    for name,scene in scene_dict.iteritems() ]
+
+    link_dict = renderManager._link_dict
+
+    links_info = { i : 
+                       {  "src" : scene_dict[src]._identifier, 
+                          "tgt" : scene_dict[tgt]._identifier }
+                  for i,(src,tgt) in link_dict.iteritems() }
+    links_json = json.dumps(links_info)
 
     with open(frame_file, "r") as i:
         template = Template(i.read())
 
     with open(html_out_file, "w") as o:
-        o.write(template.render(scenes=scenes))
+        o.write(template.render(scenes=scenes_contexts,links=links_json))
 
 if __name__ == "__main__":
     main()
--- js/fabula.js
+++ js/fabula.js
@@ -1,2 +1,14 @@
-var pong = new Howl({urls: ['autobahn.mp3']});
-$("#btn").click(function(){pong.play();});
+$(function() {
+    $(".fscene").hide()
+    for (linkid in links)
+    {
+        $("#"+linkid).click(function(link_info) { return function()
+        {
+            $("#"+link_info.src).fadeOut(1000, 
+                function() {
+                $("#"+link_info.tgt).fadeIn(1000);
+            });
+        }}(links[linkid]));
+    }
+    $("#scene0").show()
+});
