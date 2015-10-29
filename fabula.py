312d86c2e1022e4788cd9702299612436d7cdd50
Michael Borinsky <borinsky@physik.hu-berlin.de>
2015-10-29 23:19:05 +0100

    Changes

--- fabula.py
+++ fabula.py
@@ -24,6 +24,9 @@ class Node(object):
         
         return out
 
+    def meta_render( self ):
+        return { "name" : self._name }
+
     def __getitem__(self, key):
         if self._name == key:
             return self
@@ -53,7 +56,7 @@ class Scene(Node):
             def link(self, link, title, text):
                 next_id = len(link_dict)
                 link_ident = "link%d_%d" % (ident, next_id)
-                link_dict[link_ident] = ( name, link )
+                link_dict[link_ident] = link
 
                 return '<a id="%s" class="flink" href="#">%s</a>' % (link_ident, text)
 
@@ -74,8 +77,47 @@ class Scene(Node):
 
         return out
 
+    def meta_render( self ):
+        out = super(Scene, self).meta_render()
+        out['links'] = self._link_dict
+
+        return out
+
+class FabulaPic(Scene):
+    def __init__( self, ident, name, yaml_tree ):
+        self._pic = yaml_tree.pop('pic', "")
+
+        super(FabulaPic, self).__init__(ident, name, yaml_tree)
+    
+    @property
+    def visible(self):
+        return self._pic != "" or super(FabulaPic, self).visible
+
+    def meta_render( self ):
+        out = super(FabulaPic, self).meta_render()
+        out['pic'] = self._pic
+
+        return out
+
+class FabulaVid(FabulaPic):
+    def __init__( self, ident, name, yaml_tree ):
+        self._video = yaml_tree.pop('video', "")
+        print self._video
+
+        super(FabulaVid, self).__init__(ident, name, yaml_tree)
+    
+    @property
+    def visible(self):
+        return self._video != "" or super(FabulaVid, self).visible
+
+    def meta_render( self ):
+        out = super(FabulaVid, self).meta_render()
+        out['video'] = self._video
+
+        return out
+
 def FabulaTree(yaml_tree):
-    return Scene( 0, "root", yaml_tree )
+    return FabulaVid( 0, "root", yaml_tree )
 
 def main():
     frame_file = "frame.html"
@@ -93,20 +135,20 @@ def main():
         yaml_tree = yaml.load( i.read() )
 
     fabulaTree = FabulaTree( yaml_tree )
-    scenes_contexts = [ leave.render() for leave in fabulaTree if leave.visible ]
+    contexts = [ leave.render() for leave in fabulaTree if leave.visible ]
 
-    link_dict = dict(sum( (list(leave._link_dict.items()) for leave in fabulaTree), [] ))
+    meta = { leave._ident : leave.meta_render() for leave in fabulaTree if leave.visible }
 
-    links_info = { i : {  "src" : fabulaTree[src]._ident, 
-                          "tgt" : fabulaTree[tgt]._ident}
-                  for i,(src,tgt) in link_dict.iteritems() }
-    links_json = json.dumps(links_info)
+    id_map = { leave._name : leave._ident for leave in fabulaTree if leave.visible }
+
+    js_meta = json.dumps(meta)
+    js_ids = json.dumps(id_map)
 
     with open(frame_file, "r") as i:
         template = Template(i.read())
 
     with open(html_out_file, "w") as o:
-        o.write(template.render(scenes=scenes_contexts,links=links_json))
+        o.write(template.render(contexts=contexts,meta=js_meta,ids=js_ids))
 
 if __name__ == "__main__":
     main()
--- js/fabula.js
+++ js/fabula.js
@@ -1,14 +1,44 @@
 $(function() {
-    $(".fscene").hide()
-    for (linkid in links)
+    $(".fscene").hide();
+
+    function showScene( scene )
     {
-        $("#"+linkid).click(function(link_info) { return function()
+        obj = meta[scene];
+        console.log(obj)
+        if ('video' in obj)
         {
-            $("#"+link_info.src).fadeOut(500, 
-                function() {
-                $("#"+link_info.tgt).fadeIn(500);
+            $("#video").attr("src", obj['video']);
+            $("#video").fadeIn(1000);
+        }
+    }
+
+    function hideScene( scene )
+    {
+    }
+
+    function changeScene( src, tgt ) 
+    {
+        hideScene( src );
+        $("#"+src).fadeOut(500, 
+            function() {
+            showScene( tgt );
+            $("#"+tgt).fadeIn(500)
             });
-        }}(links[linkid]));
+    }
+
+    for (objid in meta)
+    {
+        obj = meta[objid];
+        for (linkid in obj.links)
+        {
+            $("#"+linkid).click(function(src, tgt)  
+            { 
+                return function()
+                {
+                    changeScene(src,tgt);
+                }
+            }( objid, ids[obj.links[linkid]] ));
+        }
     }
     $("#node1").fadeIn(1500);
 });
